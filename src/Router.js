98a2bdb5702663097dc2eace2e0dfd0ad568db60
Eunji Kang <etothej@naver.com>
2022-11-01 17:15:45 +0900

    ADD: 카카오 로그인 추가 - 수정사항 반영

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Host from './pages/Host/Host';
 import Detail from './pages/Detail/Detail';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import KakaoLogin from './pages/KakaoLogin/KakaoLogin';
 
 const Router = () => {
   return (
@@ -11,6 +11,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/host" element={<Host />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/kakao/login" element={<KakaoLogin />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
+
 const Main = () => {
   return (
     <>
