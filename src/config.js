678717628ce2a3669503b4b57b191123835baeb1
Eunji Kang <etothej@naver.com>
2022-11-08 11:09:32 +0900

    ADD: config.js 파일 생성

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const BASE_URL = ``;
+
+export const API = {
+  MAIN: `${BASE_URL}/`,
+  SIGNIN: `${BASE_URL}/`,
+  HOST: `${BASE_URL}/`,
+  BOOK: `${BASE_URL}/`,
+  DETAIL: `${BASE_URL}/`,
+  MAINFILTER: `${BASE_URL}/`,
+  GNBFILTER: `${BASE_URL}/`,
+  LNBFILTER: `${BASE_URL}/`,
+};
